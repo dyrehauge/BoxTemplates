a2ed5f94fb822700e9b75a953cf8f51115479f09
Dyrehauge <dthomasn@gmail.com>
2015-04-09 16:54:24 +0200

    boxtemplates

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ProductTemplates</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<nav>
+		<li><a href="#productTemplate-1">News</a></li>
+		<li><a href="#productTemplate-2">View Projects</a></li>
+		<li class="logoph"><a class="logo" href="#productTemplate-2"><img src="img/logo.png" alt=""></a></li>
+	</nav>
+
+	<section id="productTemplate-1">
+
+	<h1>News #1</h1>
+	
+	<div class="boxmaster">
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img src="img/banjo.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Superb Conference Barcelona</h3>
+					<a href="#">Live the great moments</a>
+					<p>Each year digital architects, pixel visionaries and curators of dreams alike, come...</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img src="img/banjo.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Superb Conference Barcelona</h3>
+					<a href="#">Live the great moments</a>
+					<p>Each year digital architects, pixel visionaries and curators of dreams alike, come...</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img src="img/banjo.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Superb Conference Barcelona</h3>
+					<a href="#">Live the great moments</a>
+					<p>Each year digital architects, pixel visionaries and curators of dreams alike, come...</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img src="img/banjo.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Superb Conference Barcelona</h3>
+					<a href="#">Live the great moments</a>
+					<p>Each year digital architects, pixel visionaries and curators of dreams alike, come...</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img src="img/banjo.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Superb Conference Barcelona</h3>
+					<a href="#">Live the great moments</a>
+					<p>Each year digital architects, pixel visionaries and curators of dreams alike, come...</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	</section>
+
+	<section id="productTemplate-2">
+
+		<h1>View Projects #1</h1>
+	
+	<div class="boxmaster">
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img src="img/banjo.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Kanduu.com</h3>
+					<h4>A Superb animal from Bronx</h4>
+					<span class="smallborder"></span>
+					<a href="#">VIEW PROJECT</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img src="img/banjo.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Kanduu.com</h3>
+					<h4>A Superb animal from Bronx</h4>
+					<span class="smallborder"></span>
+					<a href="#">VIEW PROJECT</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img src="img/banjo.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Kanduu.com</h3>
+					<h4>A Superb animal from Bronx</h4>
+					<span class="smallborder"></span>
+					<a href="#">VIEW PROJECT</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img src="img/banjo.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Kanduu.com</h3>
+					<h4>A Superb animal from Bronx</h4>
+					<span class="smallborder"></span>
+					<a href="#">VIEW PROJECT</a>
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+	</section>
+	
+</body>
+</html>
