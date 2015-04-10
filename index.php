20badb07b82ac19b6a58bae39bf4864d18fba018
Dyrehauge <dthomasn@gmail.com>
2015-04-10 22:10:26 +0200

    boxes

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<nav>
-		<li><a href="#productTemplate-1">News</a></li>
-		<li><a href="#productTemplate-2">View Projects</a></li>
-		<li class="logoph"><a class="logo" href="#productTemplate-2"><img src="img/logo.png" alt=""></a></li>
-	</nav>
+<nav>
+	<li><a href="#productTemplate-1">News</a></li>
+	<li><a href="#productTemplate-2">View Projects</a></li>
+	<li class="logoph"><a class="logo" href="#productTemplate-2"><img src="img/logo.png" alt=""></a></li>
+</nav>
 
-	<section id="productTemplate-1">
+<section id="productTemplate-1">
 
 	<h1>News #1</h1>
 	
@@ -25,7 +25,7 @@
 					<img src="img/banjo.jpg" alt="">
 				</div>
 				<div class="textwrap">
-					<h3>Superb Conference Barcelona</h3>
+					<h3>Superb LORT Barcelona</h3>
 					<a href="#">Live the great moments</a>
 					<p>Each year digital architects, pixel visionaries and curators of dreams alike, come...</p>
 				</div>
@@ -84,9 +84,9 @@
 			</div>
 		</div>
 	</div>
-	</section>
+</section>
 
-	<section id="productTemplate-2">
+<section id="productTemplate-2">
 
 		<h1>View Projects #1</h1>
 	
@@ -149,7 +149,73 @@
 
 
 	</div>
-	</section>
+</section>
+
+<section id="productTemplate-3">
+
+		<h1>View Projects #2</h1>
 	
+	<div class="boxmaster">
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img class="vertical" src="img/vertidummy.jpg" alt="">
+					<img class="horizon" src="img/horidummy.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Kanduu.com</h3>
+					<h4>A Superb animal from Bronx</h4>
+					<span class="smallborder"></span>
+					<p>Indentitet, Video, Web, Motion3D</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img class="vertical" src="img/horidummy.jpg" alt="">
+					<img class="horizon" src="img/vertidummy.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Kanduu.com</h3>
+					<h4>A Superb animal from Bronx</h4>
+					<span class="smallborder"></span>
+					<p>Indentitet, Video, Web, Motion3D</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img class="vertical" src="img/vertidummy.jpg" alt="">
+					<img class="horizon" src="img/horidummy.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Kanduu.com</h3>
+					<h4>A Superb animal from Bronx</h4>
+					<span class="smallborder"></span>
+					<p>Indentitet, Video, Web, Motion3D</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="boxwrap">
+			<div class="box">
+				<div class="imagewrap">
+					<img class="vertical" src="img/horidummy.jpg" alt="">
+					<img class="horizon" src="img/vertidummy.jpg" alt="">
+				</div>
+				<div class="textwrap">
+					<h3>Kanduu.com</h3>
+					<h4>A Superb animal from Bronx</h4>
+					<span class="smallborder"></span>
+					<p>Indentitet, Video, Web, Motion3D</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 </body>
 </html>
